16633fe098443a2e42d627a96ccf5fcdb42d82c7
yuriysurzhikov <yuriisurzhykov@gmail.com>
2023-01-28 05:00:01 +0200

    Tiny changes for gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ import java.io.FileInputStream
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id(Plugins.kotlinAndroid)
 }
 
 android {
---
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -0,0 +1,4 @@
+object Plugins {
+    const val kotlinKapt = "kotlin-kapt"
+    const val kotlinAndroid = "org.jetbrains.kotlin.android"
+}
