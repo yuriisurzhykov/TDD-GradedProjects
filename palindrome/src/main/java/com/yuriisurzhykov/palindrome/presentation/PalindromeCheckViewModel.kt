b0a5f42541ae3ebe5a5f1ea4f95868073fce6cdb
yuriysurzhikov <yuriisurzhykov@gmail.com>
2023-01-28 10:52:02 +0200

    Add dependencies module

--- palindrome/src/main/java/com/yuriisurzhykov/palindrome/presentation/PalindromeCheckViewModel.kt
+++ palindrome/src/main/java/com/yuriisurzhykov/palindrome/presentation/PalindromeCheckViewModel.kt
@@ -1,9 +1,12 @@
 package com.yuriisurzhykov.palindrome.presentation
 
 import androidx.lifecycle.ViewModel
+import com.yuriisurzhykov.palindrome.domain.IPalindromeCheckUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class PalindromeCheckViewModel @Inject constructor() : ViewModel() {
+class PalindromeCheckViewModel @Inject constructor(
+    private val useCase: IPalindromeCheckUseCase
+) : ViewModel() {
 }
