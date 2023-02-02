8556694ff0e1cea90de84937baaa92f2bcb3caa2
yuriysurzhikov <yuriisurzhykov@gmail.com>
2023-02-02 12:52:41 +0200

    MSRV_3 - string reverse use case test

--- stringReverse/src/test/java/com/yuriisurzhykov/tddgraded/stringreverse/StringReverseUseCaseTest.kt
+++ stringReverse/src/test/java/com/yuriisurzhykov/tddgraded/stringreverse/StringReverseUseCaseTest.kt
@@ -16,18 +16,26 @@
 
 package com.yuriisurzhykov.tddgraded.stringreverse
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
+class StringReverseUseCaseTest {
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `test reverse string with normal length`() {
+        val useCase = StringReverseUseCase.Base()
+        val testData = "Some string for test!"
+        val expected = "!tset rof gnirts eomS"
+        assertEquals(expected, useCase.reverse(testData))
     }
+
+    @Test
+    fun `test reverse string with empty string`() {
+        val useCase = StringReverseUseCase.Base()
+        val testData = ""
+        val expected = ""
+        assertEquals(expected, useCase.reverse(testData))
+    }
+
+
 }
